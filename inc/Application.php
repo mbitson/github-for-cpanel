940ef96982ec4c6f75e4856cfee4e196e85b8c4a
mbitson <importune@gmail.com>
2015-05-19 12:51:02 -0400

    Added basic application functions to index

--- inc/Application.php
+++ inc/Application.php
@@ -2,20 +2,18 @@
 /**
  * Application Class File
  * PHP Version 5
- * @category cPanel
- * @package  cPanel.Plugin
  * @author   NeXt I.T. - Mikel Bitson <me@mbitson.com>
  * @license  http://opensource.org/licenses/MIT	MIT License
  * @link     http://github-for-cpanel.mbitson.com
  */
 namespace GHCP;
 
+// Require the cpanel class to be loadable to integrate.
+require_once("/usr/local/cpanel/php/cpanel.php");
+
 /**
- * Application Class File
- * PHP Version 5
+ * Application Class
  * Date: 5/19/2015
- * @category cPanel
- * @package  cPanel.Plugin
  * @author   NeXt I.T. - Mikel Bitson <me@mbitson.com>
  * @license  http://opensource.org/licenses/MIT	MIT License
  * @link     http://github-for-cpanel.mbitson.com
@@ -28,12 +26,49 @@ class Application
 	protected $_github;
 
 	/**
+	 * @var \CPANEL Will contain the cpanel client when loaded.
+	 */
+	protected $_cpanel;
+
+	/**
 	 * Initialize GitHub for Cpanel Application
 	 */
 	public function __construct()
 	{
 		// Include composer autoloader.
 		require_once 'vendor/autoload.php';
+	}
+
+	/**
+	 * Start cPanel integration, output header of paper lantern.
+	 * @return bool Return status
+	 */
+	public function start()
+	{
+		// Init a cpanel instnace.
+		$this->_cpanel = new \CPANEL();
+
+		// Print header per cpanel docs (I'd prefer echo, but will remain consistant with cPanel)
+		print $this->_cpanel->header( 'GitHub' );
+
+		// Return status!
+		return TRUE;
+	}
+
+	/**
+	 * End cPanel integration, output footer of paper lantern and disconnect.
+	 * @return bool Return status
+	 */
+	public function stop()
+	{
+		// Print the footer (I'd prefer echo, but will remain consistant with cPanel)
+		print $this->_cpanel->footer();
+
+		// Disconnect the cpanel integration instance
+		$this->_cpanel->end();
+
+		// Return status!
+		return TRUE;
 	}
 
 	public function github()
--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: mbitson
+ * PHP Version 5
+ * @author   NeXt I.T. - Mikel Bitson <me@mbitson.com>
+ * @license  http://opensource.org/licenses/MIT	MIT License
+ * @link     http://github-for-cpanel.mbitson.com
  * Date: 5/19/2015
- * Time: 8:41 AM
  */
+
+// Include application class
+require_once('inc/Application.php');
+
+// Init app
+$app = new GHCP\Application();
+
+// Output header, connect
+$app->start();
 ?>
+
+	<p>Hello World!</p>
+
+<?php
+// Output footer, disconnect
+$app->stop();
+
+// End of file index.php
