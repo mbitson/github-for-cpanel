c13d04e711bd69870db9704485a5b9d0b8681386
mbitson <importune@gmail.com>
2015-05-19 13:42:11 -0400

    Continued install script, added git and composer installation and detection

--- install.sh
+++ install.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 # Installation script for GitHub for cPanel plugin.
+# Will first check dependancies and install
+
+# Git
+command -v git >/dev/null 2>&1
+GIT_IS_INSTALLED=$?
+if [[ $GIT_IS_INSTALLED -ne 0 ]]; then
+    echo >&2 "Git is not installed. Installing..";
+    yum install git
+fi
+
+#Composer
+composer -v > /dev/null 2>&1
+COMPOSER_IS_INSTALLED=$?
+if [[ $COMPOSER_IS_INSTALLED -ne 0 ]]; then
+    echo "Composer is not installed. Installing..."
+    # Install Composer
+    curl -sS https://getcomposer.org/installer | php
+    sudo mv composer.phar /usr/local/bin/composer
+else
+    echo "Composer is installed. Running update..."
+    sudo composer self-update
+fi
+
+# Download the plugin archive
 wget https://github.com/mbitson/github-for-cpanel/blob/master/build/ghcp-release.tar.gz?raw=true
+
+# Register plugin with cPanel
 /usr/local/cpanel/scripts/install_plugin ghcp-release.tar.gz
+
+# Cleanup by removing release
+rm -f ghcp-release.tar.gz
+
+# TODO - Run composer update once plugin is installed.
