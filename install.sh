fe376f629257296e359749f0ba238b07ff4f5727
mbitson <importune@gmail.com>
2015-05-19 16:32:41 -0400

    Fix install script.

--- install.sh
+++ install.sh
@@ -30,10 +30,13 @@ cd /usr/local/cpanel/base/frontend/paper_lantern/github_for_cpanel
 # Download the plugin archive
 wget -q https://github.com/mbitson/github-for-cpanel/blob/master/build/ghcp-release.tar.gz?raw=true -O github_for_cpanel.tar.gz
 
-# Extract archive & Cleanup by removing release
-tar -zxvf github_for_cpanel.tar.gz && rm -f github_for_cpanel.tar.gz
+# Extract archive
+tar -zxvf github_for_cpanel.tar.gz &&
 
 # Register plugin with cPanel
 /usr/local/cpanel/scripts/install_plugin /usr/local/cpanel/base/frontend/paper_lantern/github_for_cpanel --theme paper_lantern
+
+#Cleanup by removing release
+rm -f /usr/local/cpanel/base/frontend/paper_lantern/github_for_cpanel/github_for_cpanel.tar.gz
 
 # TODO - Run composer update once plugin is installed.
